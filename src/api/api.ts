4ace4c3daacde8690ad33c962d21bfea8515acab
Raysa Neres <raysa.santos@estudante.ifms.edu.br>
2025-10-21 11:50:54 -0400

    #atualizações

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,28 +1,31 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL:import.meta.env.VITE_BASE_URL
+    baseURL:import.meta.env.VITE_API_URL
 })
-//Middleware -> Interceptors 
-//1 - interceptors de requisição: inclui o token em cada requisição
-api.interceptors.request.use((config) => {
+
+//Middleware -> interceptors
+
+//1 - interceptors de requesição: inclui o token em cada requisição
+api.interceptors.request.use((config)=>{
     const token = localStorage.getItem("token")
     if(token)
         config.headers.Authorization = `Bearer ${token}`
-        return config
-    }
-  )   
+    return config
+})
 
-//Interceptor de resposta: quando o backend retornar 401, redireciona para a página de login
+//Interceptor de resposta:
+//quando o backend retornar 401 redireciona para o login
 api.interceptors.response.use(
-    (response)=> response,
-    (error) => {
+    (response)=>response,
+    (error)=>{
         const status = error?.response?.status
         if(status===401&&!(error?.response?.config?.url.endsWith("/login"))){
             localStorage.removeItem("token")
             window.location.href = "/login?mensagem=Token_expirado!"
         }
         return Promise.reject(error)
-     }
+    }
 )
+
 export default api;
